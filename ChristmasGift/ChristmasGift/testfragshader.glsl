43363f0efccba6edf0d5e44670ebcb2aadda4466
Xiaoyu Ma <maxiaoyuzdz@gmail.com>
2011-11-24 23:51:26 +0000

    [add]generated from shader app

--- ChristmasGift/ChristmasGift/testfragshader.glsl
+++ ChristmasGift/ChristmasGift/testfragshader.glsl
@@ -5,5 +5,5 @@ in vec4 Color;
 void main() {
     //FragColor = vec4(Color, 1.0);
 	gl_FragColor = Color;
-	//gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	//gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
 }
--- ChristmasGift/ChristmasGift/testvertexshader.glsl
+++ ChristmasGift/ChristmasGift/testvertexshader.glsl
@@ -4,10 +4,11 @@
 in vec3 VertexPosition;
 in vec4 VertexColor;
 out vec4 Color;
+
 void main()
 {
 	//vec4 tempcolor = VertexColor;//vec4(1.0,1.0,0,1);
 	Color = VertexColor;//tempcolor;//vec4(1.0, 0.0, 0.0, 1.0);
 	gl_Position = ftransform();
-	
+
 }
