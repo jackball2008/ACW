cccb567837c1bb7a585eb0a7f2780ec95870155b
Xiaoyu Ma <maxiaoyuzdz@gmail.com>
2011-12-08 21:59:50 +0000

    [check]

--- GameEngine/Semaphore Hero/SemaphoreHeroGameSceneManager.cpp
+++ GameEngine/Semaphore Hero/SemaphoreHeroGameSceneManager.cpp
@@ -17,8 +17,26 @@ void SemaphoreHeroGameSceneManager::RunGamePlaying(int* gs){
 	playScene->Running();
 	_renderManager->RenderOpenGL(playScene);
 }
+//choose main menu
+void SemaphoreHeroGameSceneManager::RunChooseMenu(int* gs){
+	_renderManager->RenderOpenGL(mainMenu);
+	key = _inputManager->GetvKeyValue();
+	switch(key){
+		case '1':
+		_inputManager->RecovervKey();
+		mainMenu->End();
+		*gs = 1;
+		break;
+	case '2':
+		_inputManager->RecovervKey();
+		mainMenu->End();
+		*gs = 2;
+		break;
+
+	}
+}
 
 //SHOWSCORE
-void SemaphoreHeroGameSceneManager::RunShowScore(int* gs){}
+//void SemaphoreHeroGameSceneManager::RunShowScore(int* gs){}
 //GAMEEXIT
-void SemaphoreHeroGameSceneManager::RunGameExit(int* gs){}
+//void SemaphoreHeroGameSceneManager::RunGameExit(int* gs){}
