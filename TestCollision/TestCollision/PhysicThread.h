8909d4d8452a888a69defe8b96cd8a3c88769cca
XiongYi <bearonexy@qq.com>
2012-04-27 20:06:45 +0100

    if any thing wrong reload this version

--- TestCollision/TestCollision/PhysicThread.h
+++ TestCollision/TestCollision/PhysicThread.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "ShareMem.h"
 #include <Windows.h>
-
+#include "RigidBody.h"
 using std::iostream;
 
 class PhysicThread :
@@ -12,8 +12,17 @@ class PhysicThread :
 public:
 	PhysicThread(void);
 	~PhysicThread(void);
+	ShareMem* _shareobject;
+
+
+	Vector      vCollisionNormal;           // the collision normal returned by our collision detection routine
+	Vector		vCollisionTangent;			// the collision tangent returned by our collision detection routine
+	Vector		vCollisionPoint;			// the world space point of collision
+	Vector		vRelativeVelocity;			// the world space relative velocity of the two bodies at the point of collision
+
+	
 private:
-	ShareMem *_shareobject;
+
 
 	HANDLE thread;
 };
