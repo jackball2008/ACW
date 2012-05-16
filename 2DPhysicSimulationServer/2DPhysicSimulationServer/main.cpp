1755d41afee0440a1b12e1830467953dfe6180b6
Xiaoyu Ma <maxiaoyuzdz@gmail.com>
2012-05-16 04:21:10 +0100

    [modified]

--- 2DPhysicSimulationServer/2DPhysicSimulationServer/main.cpp
+++ 2DPhysicSimulationServer/2DPhysicSimulationServer/main.cpp
@@ -73,10 +73,11 @@ int main()
 		cerr << "Listen failed with " << WSAGetLastError()  << endl;
 	} else {
 		socketok = true;
-		char buffer[256];
+		char buffer[128];
 
 		while(1)
 		{
+			Sleep(10);
 			SOCKET s1 = accept(s,NULL,NULL);
 			//cout<<"s1 = "<<s1<<endl;
 			if (s1==INVALID_SOCKET)
@@ -85,7 +86,7 @@ int main()
 			}
 			else 
 			{
-				int recres = recv(s1,buffer,256,0);
+				int recres = recv(s1,buffer,128,0);
 				//cout<<"recres = "<<recres<<endl;
 				if(recres==SOCKET_ERROR)
 				{
@@ -96,7 +97,7 @@ int main()
 					//cout<<"receive ok"<<buffer<<endl;
 
 					//start to analyze
-					istrstream sin(buffer, 256-1);
+					istrstream sin(buffer, 128-1);
 					string word;
 					sin>>word;
 					//int type = -2;
@@ -104,8 +105,43 @@ int main()
 					if(word.compare("s") == 0)
 					{
 						//receive a send position data
-						//cout<<"+++"<<endl;
-						// 
+						//cout<<"+++"<<buffer<<endl;
+						// read id
+						sin>>word;
+						int sender_id = atof(word.c_str());
+
+						if(sender_id!=tempData.sender_id)
+						{
+							//x
+							sin>>word;
+							float pos_x = (float)atof(word.c_str());
+
+							//y
+							sin>>word;
+							float pos_y = (float)atof(word.c_str());
+
+							//v_x
+							sin>>word;
+							float v_x = (float)atof(word.c_str());
+
+							//v_y
+							sin>>word;
+							float v_y = (float)atof(word.c_str());
+
+							cout<<" x = "<<pos_x<<" y = "<<pos_y<<" vx = "<<v_x<<" vy = "<<v_y<<endl;
+
+							tempData.sender_id = sender_id;
+							tempData.pos_x = pos_x;
+							tempData.pos_y = pos_y;
+							tempData.velocity_x = v_x;
+							tempData.velocity_y = v_y;
+						}
+						else
+						{
+							cout<<"resend..."<<endl;
+						}
+						
+
 
 
 					}
@@ -142,7 +178,7 @@ int main()
 							ss_v_y<<tempData.velocity_y;
 							string v_y_str = ss_v_y.str();
 
-							string datastr = " ";
+							string datastr = " r ";
 
 							datastr.append(x_str);
 							datastr.append(" ");
@@ -157,11 +193,20 @@ int main()
 
 
 							//(char*)datastr.c_str()
-
+							cout<<"send feed back = "<<datastr<<endl;
 							//s1
 							if (send(s1, (char*)datastr.c_str(), (int)strlen((char*)datastr.c_str()), 0)== SOCKET_ERROR)
 							{
 								cerr << "Send Request failed with " << WSAGetLastError() << endl;
+							}
+							else
+							{
+								//reset temp
+								tempData.sender_id = -99;
+								tempData.pos_x = -99;
+								tempData.pos_y = -99;
+								tempData.velocity_x = -99;
+								tempData.velocity_y = -99;
 							}
 
 
